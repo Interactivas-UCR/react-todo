3f2ac4e9818c3ac2bdacbb909c7f55ba2e70fecf
Nick Gonzalez <nick.gonse@gmail.com>
2020-02-15 19:32:19 -0600

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,7 @@ const Footer = () => {
 				<div className="row">
 					<div className="col-md-6 mt-md-0 mt-3">
 						<h5 className="text-uppercase">Footer Content</h5>
-						<p>
-							Here you can use rows and columns to organize your
-							footer content.
-						</p>
+						<p>Here you can use rows and columns to organize your footer content.</p>
 					</div>
 					<hr className="clearfix w-100 d-md-none pb-3" />
 					<div className="col-md-3 mb-md-0 mb-3">
@@ -27,6 +24,9 @@ const Footer = () => {
 							</li>
 							<li>
 								<a href="#!">Link 4</a>
+							</li>
+							<li>
+								<a href="#!">Link 5</a>
 							</li>
 						</ul>
 					</div>
@@ -49,9 +49,7 @@ const Footer = () => {
 					</div>
 				</div>
 			</div>
-			<div className="footer-copyright text-center py-3">
-				© 2018 Copyright
-			</div>
+			<div className="footer-copyright text-center py-3">© 2018 Copyright</div>
 		</footer>
 	);
 };
